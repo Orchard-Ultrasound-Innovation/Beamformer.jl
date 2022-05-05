a1ec570cb626c36a241b80244d273dad3cdc2d99
Documenter.jl <documenter@juliadocs.github.io>
2022-05-05 17:12:27 +0000

    build based on 2ab07b1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Beamformer","category":"page"},{"location":"#Beamformer","page":"Home","title":"Beamformer","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Beamformer.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Beamformer]","category":"page"}]
+}
